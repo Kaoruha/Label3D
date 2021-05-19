ec4ee928759ecd6aba8453fae3c567ff858449c0
Sunyu <809186577@qq.com>
2021-05-19 14:03:29 +0800

    FontColor Change

--- addons/Label3D/3d/Label3D.cs
+++ addons/Label3D/3d/Label3D.cs
@@ -4,64 +4,64 @@ using System;
 [Tool]
 public class Label3D : Spatial
 {
-	[Export]
-	public NodePath viewpath;
-	private Viewport viewport = null;
-	[Export]
-	public NodePath labelpath;
-	private Label2D label2D = null;
+    [Export]
+    public NodePath viewpath;
+    private Viewport viewport = null;
+    [Export]
+    public NodePath labelpath;
+    private Label2D label2D = null;
 
-	[Export]
-	public NodePath meshpath;
-	private MeshInstance mesh = null;
+    [Export]
+    public NodePath meshpath;
+    private MeshInstance mesh = null;
+
+    private SpatialMaterial mat = null;
 
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _EnterTree()
-	{
-		GD.Print("Label3D EnterTree");
-		GD.Print("Label3D EnterTree Complete");
-	}
-	public override void _Ready()
-	{
-		GD.Print("Label3D Get Ready");
-		Initialization();
-		GD.Print("Label3D Get Ready Complete");
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _EnterTree()
+    {
+        GD.Print("Label3D EnterTree");
+        GD.Print("Label3D EnterTree Complete");
+    }
+    public override void _Ready()
+    {
+        GD.Print("Label3D Get Ready");
+        Initialization();
+        GD.Print("Label3D Get Ready Complete");
+    }
 
-	public override void _Process(float delta)
-	{
-	}
+    public override void _Process(float delta)
+    {
+    }
 
-	private void Initialization()
-	{
-		viewport = GetNode<Viewport>(viewpath);
-		label2D = (Label2D)GetNode<Label2D>(labelpath);
-		mesh = GetNode<MeshInstance>(meshpath);
-	}
-	public Label2D GetLabel()
-	{
-		if (label2D == null)
-		{
-			Initialization();
-		}
-		return label2D;
-	}
+    private void Initialization()
+    {
+        viewport = GetNode<Viewport>(viewpath);
+        label2D = (Label2D)GetNode<Label2D>(labelpath);
+        mesh = GetNode<MeshInstance>(meshpath);
+        mat = (SpatialMaterial)mesh.GetActiveMaterial(0);
+    }
+    public Label2D GetLabel()
+    {
+        if (label2D == null)
+        {
+            Initialization();
+        }
+        return label2D;
+    }
 
-	public Viewport GetViewPort()
-	{
-		if (viewport == null)
-		{
-			Initialization();
-		}
-		return viewport;
-	}
+    public Viewport GetViewPort()
+    {
+        if (viewport == null)
+        {
+            Initialization();
+        }
+        return viewport;
+    }
 
-	public void SetMatColor(Color color)
-	{
-		GD.Print(color);
-		var mat = mesh.GetActiveMaterial(0);
-		// TODO 设置材质球的颜色
-		GD.Print(mat);
-	}
+    public void SetMatColor(Color color)
+    {
+        mat.AlbedoColor = color;
+    }
 }
